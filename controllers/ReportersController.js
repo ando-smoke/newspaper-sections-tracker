9301c7ee80a28bd0b7a9df7a7d549735b5f288bc
Andy Uppendahl <a.uppendahl@gmail.com>
2015-02-27 14:14:04 -0800

    Add ReportersController to project.

---
+++ controllers/ReportersController.js
@@ -0,0 +1,20 @@
+newspaperSections.controller('ReportersCtrl',
+  function ReportersCtrl($scope, $stateParams, SectionsFactory, UtilitiesFactory) {
+
+  $scope.section = UtilitiesFactory.findByID(SectionsFactory.sections, $stateParams.sectionID);
+  $scope.addReporter = function() {
+    $scope.sections.reporters.push({
+      firstName: $scope.firstName,
+      lastName: $scope.lastName,
+      displayName: $scope.firstName + " " + $scope.lastName,
+      birthDate: $scope.birthDate,
+      employmentYears: $scope.employmentYears
+    });
+
+    $scope.firstName = null;
+    $scope.lastName = null;
+    $scope.birthDate = null;
+    $scope.employmentYears = null;
+  };
+
+});
