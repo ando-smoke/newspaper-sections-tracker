ebd22d421bf1da984338e8564ae98d7c2be802f9
Andy Uppendahl <a.uppendahl@gmail.com>
2015-02-27 13:52:56 -0800

    Add functionality for adding new section.

--- controllers/SectionsController.js
+++ controllers/SectionsController.js
@@ -1,4 +1,14 @@
-newspaperSections.controller('SectionsCtrl', function SectionsCtrl($scope, SectionsFactory) {
+newspaperSections.controller('SectionsCtrl',
+  function SectionsCtrl($scope, SectionsFactory) {
+
   $scope.sections = SectionsFactory.sections;
   $scope.SectionsFactory = SectionsFactory;
+  $scope.addSection = function() {
+    $scope.sections.push({
+      id: $scope.sections.length + 1,
+      name: $scope.sectionName
+    });
+
+    $scope.sectionName = null;
+  };
 });
