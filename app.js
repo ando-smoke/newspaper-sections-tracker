3c0352a8bd567110b4c001754af8556f4827141e
Andy Uppendahl <a.uppendahl@gmail.com>
2015-02-27 13:09:18 -0800

    Add 'home' state to app.js.

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 var newspaperSections = angular.module('newspaperSections', ['ui.router']);
+
+newspaperSections.config(function($stateProvider) {
+  $stateProvider.state('home', {
+    url: "",
+    templateUrl: "partials/home.html"
+  });
+});
